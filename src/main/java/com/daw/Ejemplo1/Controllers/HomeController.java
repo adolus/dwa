f3726767dc17d73577d04bb95b02dbe0202e350c
SALA-C <SALA-C@CC-C-30.itssnp.edu>
2022-05-11 15:44:01 -0500

    Primera vez

---
+++ src/main/java/com/daw/Ejemplo1/Controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.daw.Ejemplo1.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String Saludo() {
+		return "saludo";
+	}
+
+}
